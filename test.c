43d093b9dfa5e9209f415d1a05e346efe1edaf08
mstih <mstih@proton.me>
2022-12-14 13:19:16 +0100

    almost works

--- test.c
+++ test.c
@@ -1,66 +1,89 @@
 #include <stdio.h>
 
-void encode(char original[], char passwd[]);
+void encode(char original[], char passwd[], char encoded[]);
 
 
 int main(){
     char original[100];
     char password[100];
-    int characters[62];
+    char encoded[100];
     printf("Enter your message: ");
-    fgets(original, sizeof(original), stdin);
-    encode(original, password);
-    printf("Your encoded message: %s", original);
+    fgets(original, 100, stdin);
+    printf("Enter a password: ");
+    fgets(password, 100, stdin);
+    encode(original, password, encoded);
+    printf("Your encoded message: %s", encoded);
 }
 
-void encode(char original[], char passwd[]){
-    int characters[62]; //array with lowercase, upercase letters and numbers
+void encode(char original[], char passwd[], char encoded[]){
+    //makes an array of characters, including numbers, lowercase letters and uppercase letters, mixed positions with eachother.
+    int characters[62]; 
     int number = 57;
-    for (int i = 1; i > 10; i+2){
+    for (int i = 1; i < 10; i+=2){
         characters[i] = number;
         number--;
     }
-    for (int i = 60; i > 53; i-2){
+    for (int i = 60; i > 51; i-=2){
         characters[i] = number;
         number--;
     }
     int lower_case = 97;
-    for (int i = 0; i < 51; i+2){
+    for (int i = 0; i < 51; i+=2){
         characters[i] = lower_case;
         lower_case++;
     }
     int upper_case = 65;
-    for(int i = 61; i > 10; i-2){
+    for(int i = 61; i > 10; i-=2){
         characters[i] = upper_case;
         upper_case++;
+    }
+
+    //makes an array with first 10 positions being numbers, then uppercase letters and after that lowercase letters.
+    int array[61];
+    int idx_num = 48;
+    for(int i = 0; i<10; i++){
+        array[i] = idx_num;
+        idx_num++;
+    }
+
+    int idx_uppercase = 65;
+    for(int i = 10; i < 37; i++){
+        array[i] = idx_uppercase;
+        idx_uppercase++;
+    }
+
+    int idx_lowercase = 97;
+    for(int i = 37; i<62; i++){
+        array[i] = idx_lowercase;
+        idx_lowercase++;
     }
 
     //main encoding loop
     char current_char;
     char current_passwd_char;
     int index = 0;
-    while(original[index] != '\0'){
-        current_char = original[index];
-
-
-
-        int passwd_index = 0;
-        while(passwd[passwd_index + 1 ] != '\0'){
-            current_passwd_char = passwd[passwd_index];
-            if(passwd[passwd_index + 1] != '\0'){
-                passwd_index++;
-            }else{
+    int passwd_index = 0;
+    while(original[index + 1] != '\0'){
+        current_char = original[index]; //defines the current character of original message
+        if(current_char != ' '){
+            if(passwd[passwd_index + 1 ] != '\0'){ //defines the current character of password
+                current_passwd_char = passwd[passwd_index];
+                passwd_index++; 
+            }else{ //if password is shorter than the message it repeats the password until needed
+                current_passwd_char = passwd[passwd_index];
                 passwd_index = 0;
             }
+
+            int difference = array[current_char] - array[current_passwd_char]; //finds a difference between position of message character and password character
+            if (difference < 0){ //if difference is negative it changes it to positive
+                difference = (-1)*difference;
+            }   
+            //printf("Password char: %i, Character char: %i, Difference: %i\n", current_passwd_char, current_char, difference);
+
+            encoded[index] = characters[difference]; //finds character in mixed array at position of difference and encodes first character as this character from mixed array
+        }else{
+            encoded[index] = original[index];
         }
-
-        int difference = (int) current_char - (int) current_passwd_char;
-
-    
-
-
-
-
-    index++;
+        index++;
     }
 }
--- test2.c
+++ test2.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    char characters[62];
-    int number = 9;
+    int characters[62]; 
+    int number = 57;
     for (int i = 1; i > 10; i+2){
-        characters[i] = number - 1;
+        characters[i] = number;
         number--;
     }
-    for(int i = 54; i < 61; i+2){
-        characters[i] = number - 1;
+    for (int i = 60; i > 53; i-2){
+        characters[i] = number;
         number--;
+    }
+    int lower_case = 97;
+    for (int i = 0; i < 51; i+2){
+        characters[i] = lower_case;
+        lower_case++;
+    }
+    int upper_case = 65;
+    for(int i = 61; i > 10; i-2){
+        characters[i] = upper_case;
+        upper_case++;
     }
 
-    printf("Printing values: \n");
+    printf("Mixed: ");
     for(int i = 0; i < 62; i++){
-        printf("%i | %i\n", i, characters[i]);
+        printf("%i, ", characters[i]);
     }
- 
+    printf("\n");
 }
